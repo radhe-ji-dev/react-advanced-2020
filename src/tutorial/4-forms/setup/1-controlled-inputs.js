54680bae544abea7279c19e31e27ce28362824c4
Radhakant Tiwari <therktiwadi@gmail.com>
2021-12-26 14:23:33 +0530

    use state counter

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,32 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [fname, setFname] = useState("");
+  const [email, setEmail] = useState("");
+
+  const clickhandle = (e) => {
+    e.preventDefault();
+    console.log("btn clicked");
+  };
+  return (
+    <>
+      <article>
+        <form className="form">
+          <div className="form-control">
+            <label htmlFor="fisrtname"> NAME</label>
+            <input type="text" id="fisrtname" name="firsrname"></input>
+          </div>
+          <div className="form-control">
+            <label htmlFor="fisrtname">Email</label>
+            <input type="text" id="email" name="email"></input>
+            <button type="submit" className="btn" onClick={clickhandle}>
+              click me
+            </button>
+          </div>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
